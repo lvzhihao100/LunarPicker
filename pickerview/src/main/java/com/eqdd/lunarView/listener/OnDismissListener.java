584deabd92d73c47f6ff20a378905176f2b05eb7
lvzhihao <1030753080@qq.com>
2017-06-10 11:43:36 +0800

    更改包名

---
+++ pickerview/src/main/java/com/eqdd/lunarView/listener/OnDismissListener.java
@@ -0,0 +1,8 @@
+package com.eqdd.lunarView.listener;
+
+/**
+ * Created by Sai on 15/8/9.
+ */
+public interface OnDismissListener {
+    public void onDismiss(Object o);
+}
---
+++ pickerview/src/main/java/com/eqdd/lunarView/listener/OnItemSelectedListener.java
@@ -0,0 +1,6 @@
+package com.eqdd.lunarView.listener;
+
+
+public interface OnItemSelectedListener {
+    void onItemSelected(int index);
+}
